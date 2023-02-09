17299786e7a51e526aa2ac0f0bb74556d0166696
Ewerton Carvalho <106186458+EwertonCBO@users.noreply.github.com>
2023-02-09 19:24:01 -0300

    Exercício cálculo de volume

---
+++ calcular volume.cpp
@@ -0,0 +1,15 @@
+// exercício h pg 26 lista de exercícios //
+#include <iostream>
+using namespace std;
+
+int main(){
+  float Volume, Comprimento = 10, Largura = 6, Altura = 1.75; 
+  
+  cout << "\nPara calcular o volume devemos utilizar a fórmula Volume = Comprimento * Largura * Altura.\n";
+  
+  Volume = Comprimento * Largura * Altura;
+
+  cout << "\n O Resultado dessa operação é: " << Volume <<"m³";
+
+  return 0; 
+}
